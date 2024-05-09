05ce13c5a78bdb773cb0d2b73fbeb1a99ae9c1e2
Petra Varkonyi <petravx@gmail.com>
2024-05-09 22:35:13 +0200

    Additional summary code

--- analysis.R
+++ analysis.R
@@ -4,4 +4,10 @@ library(tidyverse)
 penguins <- read_csv("penguins.csv")
 
 penguins |>
-  count(species)
+  count(island)
+
+penguins |>
+  select(-(bill_depth_mm)) |>
+  filter(sex == "female") |>
+  group_by(island) |>
+  summarise(mean_bill_length = mean(bill_length_mm))
